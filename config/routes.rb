8ebe0f034a5406a5c952b749c6bbd0c103b8e600
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-03-23 15:11:40 -0700

    fixed routes from default paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: 'animals#index'
-  get 'animals/index', to: 'animals#index', as: 'animals'
-  get 'animals/:id/show/', to: 'animals#show', as: 'animal'
-  get 'animals/new', to: 'animals#new', as: 'new_animal'
-  post 'animals/create', to: 'animals#create'
-  get 'animals/:id/edit', to: 'animals#edit', as: 'edit_animal'
-  patch 'animals/:id/update', to: 'animals#update'
-  delete 'animals/:id/destroy', to: 'animals#destroy'
+  get '/animals', to: 'animals#index', as: 'animals'
+  get '/animals/:id', to: 'animals#show', as: 'animal'
+  get '/animals/new', to: 'animals#new', as: 'new_animal'
+  post '/animals', to: 'animals#create'
+  get '/animals/:id/edit', to: 'animals#edit', as: 'edit_animal'
+  patch '/animals/:id', to: 'animals#update'
+  delete '/animals/:id', to: 'animals#destroy'
 end
