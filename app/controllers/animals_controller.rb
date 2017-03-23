b9fcac088e19fa79611e5d9d7816763227aace82
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-03-23 10:06:15 -0700

    wrote methods for controller

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,22 +1,47 @@
 class AnimalsController < ApplicationController
   def index
+    @animals = Animal.all
   end
 
   def show
+    @animal = Animal.find(params[:id])
   end
 
   def edit
-  end
-
-  def create
-  end
-
-  def new
-  end
-
-  def destroy
+    @animal = Animal.find(params[:id])
   end
 
   def update
+    @animal = Animal.find(params[:id])
+    if @animal.update(animal_params)
+      redirect_to animals_path
+    else
+      render :edit
+    end
   end
+
+  def create
+    @animal = Animal.create(animal_params)
+  end
+
+  def new
+    @animal = Animal.create(animal_params)
+    if @animal.save
+      redirect_to animals_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @animal = Animal.find(params[:id]).destroy
+    redirect_to animals_path
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :species, :age, :image)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
-  get 'animals/index'
-
-  get 'animals/show'
-
-  get 'animals/edit'
-
-  get 'animals/create'
-
-  get 'animals/new'
-
-  get 'animals/destroy'
-
-  get 'animals/update'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'animals#index'
+  get 'animals/index', to: 'animals#index', as: 'animals'
+  get 'animals/:id/show/', to: 'animals#show', as: 'animal'
+  get 'animals/new', to: 'animals#new', as: 'new_animal'
+  post 'animals/create', to: 'animals#create'
+  get 'animals/:id/edit', to: 'animals#edit', as: 'edit_animal'
+  patch 'animals/:id/update', to: 'animals#update'
+  delete 'animals/:id/destroy', to: 'animals#destroy'
 end
