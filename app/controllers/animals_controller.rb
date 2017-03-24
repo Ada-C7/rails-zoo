36ea45e772e453b66e80fcd055c3f8477dd16685
Alix Hamilton <alix.hamilton@gmail.com>
2017-03-24 10:31:43 -0700

    adds create

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -4,9 +4,16 @@ class AnimalsController < ApplicationController
   end
 
   def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      redirect_to animals_index_path
+    else
+      render :new
+    end
   end
 
   def new
+    @animal = Animal.new
   end
 
   def edit
@@ -19,5 +26,11 @@ class AnimalsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :species, :scientific_name, :age, :photo_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  get 'animals/index'
-
-  get 'animals/create'
-
-  get 'animals/new'
-
-  get 'animals/edit'
-
-  get 'animals/show'
-
-  get 'animals/update'
-
-  get 'animals/destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :animals
 
