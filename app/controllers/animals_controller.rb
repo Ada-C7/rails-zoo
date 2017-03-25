96ad85f79392ebad26563c4f198b4debeb5b5cb2
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-03-25 15:04:55 -0700

    created new route and new methods

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,12 +1,27 @@
 class AnimalsController < ApplicationController
+
+  # View all animals
   def index
     @animals = Animal.all
   end
 
+  # View one animal
   def show
     id = params[:id].to_i
     @animal = Animal.find(id)
   end
 
+  # Create an animal
+  def new
+    @animal = Animal.new
+  end
+  def create
+    Animal.create(animal_params)
+    redirect_to animals_path
+  end
 
+  private
+  def animal_params
+    return params.require(:animal).permit(:name, :species, :age)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  # View all animals
   get 'animals', to: 'animals#index', as: 'animals'
+  # View one animal
   get 'animals/:id', to: 'animals#show', as: 'animal'
+
+  #Create an animal
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
