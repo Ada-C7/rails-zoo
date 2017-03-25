ef0c408d775d6efb2810b79474218761af4d931c
Tehut Getahun <tehut.m.g@gmail.com>
2017-03-24 22:34:38 -0700

    added methods to controller:create and private method task_param

--- animalsapp/app/controllers/animals_controller.rb
+++ animalsapp/app/controllers/animals_controller.rb
@@ -1,7 +1,23 @@
 class AnimalsController < ApplicationController
   def index
+    @animals = Animal.all
   end
 
   def new
+    @animal = Animal.new
   end
+
+  def create
+  Task.create(task_params)
+  redirect_to tasks_path
+  end
+
+
+
+
+private
+def task_params
+  return params.require(:task).permit(:name, :deadline, :completed)
+end
+
 end
