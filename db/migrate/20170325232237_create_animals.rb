329385c2126437c341c82f7073550931b0a78c07
laura <lmelgarejos@unal.edu.co>
2017-03-25 16:30:57 -0700

    created animals controller and animal model

---
+++ db/migrate/20170325232237_create_animals.rb
@@ -0,0 +1,12 @@
+class CreateAnimals < ActiveRecord::Migration[5.0]
+  def change
+    create_table :animals do |t|
+      t.string :name
+      t.string :species
+      t.integer :age
+      t.integer :weight
+
+      t.timestamps
+    end
+  end
+end
