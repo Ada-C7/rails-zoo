81a0c9c911f825b177a971dcde47a7e250929e22
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-03-25 14:12:19 -0700

    Links on index go to show individual details. Still weird extra info haning on index page

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -2,4 +2,11 @@ class AnimalsController < ApplicationController
   def index
     @animals = Animal.all
   end
+
+  def show
+    id = params[:id].to_i
+    @animal = Animal.find(id)
+  end
+
+
 end
