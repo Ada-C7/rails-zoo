999f40bc80dfa81477a9f5d8d84950b89feb909d
Kelly Souza <kellysouza99@gmail.com>
2017-03-26 11:32:35 -0700

    Adds index, show and new

--- zoo-app/app/controllers/animals_controller.rb
+++ zoo-app/app/controllers/animals_controller.rb
@@ -1,4 +1,29 @@
 class AnimalsController < ApplicationController
   def index
+    @animals = Animal.all
   end
+
+  def show
+    @animal = Animal.find(params[:id])
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.create animal_params
+    if @animal.save
+      redirect_to animals_path
+    end
+  end
+
+
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :species, :age, :snack)
+  end
+
 end
--- zoo-app/config/routes.rb
+++ zoo-app/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'animals/index'
 
+
+  get '/animals/new', to: 'animals#new', as: 'new_animal'
+  post '/animals/', to: 'animals#create'
+
+  get '/animals', to: 'animals#index'
+  
+  get '/animals/:id', to: 'animals#show', as: 'animal'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
