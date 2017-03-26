ce51fa2266b0cdd6371599e7cf1881e6c895b0ce
natalia-ku <bilberryfm@mail.ru>
2017-03-25 20:19:46 -0700

    added controller for show,edit,newm,delete

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,40 @@
+class AnimalsController < ApplicationController
+  def index
+    @animals = Animal.all
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    Animal.create(animal_params)
+    redirect_to animals_path
+  end
+
+  def show
+    @animal = Animal.find(params[:id])
+  end
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    animal = Animal.find(params[:id])
+    animal.update_attributes(animal_params)
+    animal.save
+    redirect_to animal_path(animal)
+  end
+
+  def destroy
+  @animal = Animal.find(params[:id])
+  @animal.destroy
+  redirect_to animals_path
+end
+
+
+  private
+  def animal_params
+    return params.require(:animal).permit(:name, :species, :age, :image)
+  end
+end
