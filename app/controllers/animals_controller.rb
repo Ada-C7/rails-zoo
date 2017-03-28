6f86cf132dd32844ca2927531b757cf61506e2b5
Marisol Lopez <lmarisolopez@gmail.com>
2017-03-28 12:20:11 -0700

    moved show action to bottom, fixed syntac for task_params

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -3,7 +3,21 @@ class AnimalsController < ApplicationController
     @animals = Animal.all
   end
 
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    Animal.create(task_params)
+  end
+
   def show
     @animal = Animal.find(params[:id])
   end
+
+  private
+  def task_params
+    return params.require(:animal).permit(:name, :species, :age)
+  end
+
 end
