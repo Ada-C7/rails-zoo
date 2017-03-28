8dc0697ca7cd76703dc8fd3238c0459fe9ddc93e
add2point71dots <alcland@gmail.com>
2017-03-27 21:28:04 -0700

    adds ability to create new animal

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -6,4 +6,19 @@ class AnimalsController < ApplicationController
   def show
     @animal = Animal.find(params[:id])
   end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    animal = Animal.create animal_params
+    redirect_to animals_path unless animal.id == nil
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :species, :age)
+  end
 end
