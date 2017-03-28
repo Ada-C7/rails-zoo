454e9c68bd02a2ecc0a189aa4b9b6b5b06afe829
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-03-27 22:33:40 -0700

    update/edit method added

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -20,6 +20,18 @@ class AnimalsController < ApplicationController
     redirect_to animals_path
   end
 
+  #Edit an animal
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    animal = Animal.find(params[:id])
+    animal.update(animal_params)
+
+    redirect_to animal_path(animal)
+  end
+
   # Create an animal needs authentication, "the Internet is a bad place"
   private
   def animal_params
