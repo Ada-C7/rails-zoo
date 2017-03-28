ac6768a95e409b6eaa115037d29263713a638444
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-03-28 11:23:18 -0700

    Updated controller#action and routes to create new form #new and to save the information #create. Now I can render /new empty page since I don't have the new template

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -15,15 +15,21 @@ class AnimalsController < ApplicationController
     animal = Animal.find(params[:id])
     if animal.update(animal_params)
       redirect_to animals_path
-    end  
-
+    end
   end
 
   def new
-
+    @animal = Animal.new
   end
 
   def create
+    animal = Animal.create(animal_params)
+    unless animal.id == nil
+      redirect_to animals_path
+    end  
+    # if animal.create(animal_params)
+    #   redirect_to animals_path
+    # end
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # root "animals/index"
 
-  # get "/animals/new", to: "animals#new"
-  # post "animals", to: "animals#create"
+  get "/animals/new", to: "animals#new"
+  post "animals", to: "animals#create"
 
   get "/animals/:id/edit", to: "animals#edit",  as:"edit_animal"
   patch "animals/:id", to: "animals#update"
