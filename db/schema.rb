c9a0f06e7ca373559a722601da447a0e922df4da
Ann Dai <gofarann@gmail.com>
2017-03-28 15:59:13 -0700

    added location as column in active records

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328224900) do
+ActiveRecord::Schema.define(version: 20170328225536) do
 
   create_table "animals", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170328224900) do
     t.string   "fave_food"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "location"
   end
 
 end
