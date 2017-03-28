93a1d2128535f482aaa13b0902f17e742fa4c47e
Marisol Lopez <lmarisolopez@gmail.com>
2017-03-28 12:08:07 -0700

    added route for new method

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ get "/animals", to: "animals#index", as: "animals"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get "/animals/:id", to: "animals#show", as: "animal"
 
+get "/animals/new", to: "animals#new", as: "new_animal"
 post "/animals", to: "animals#create"
 
 end
