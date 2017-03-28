3b04e67ef10c32088c8aad629f4b501d2cc7e8d3
Marisol Lopez <lmarisolopez@gmail.com>
2017-03-27 19:18:54 -0700

    added functionality to view one specific animal's page, changed some of route names

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # get 'animals/index'
 
-get "/animals", to: "animals#index", as: "home"
+get "/animals", to: "animals#index", as: "animals"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+get "/animals/:id", to: "animals#show", as: "animal"
 
 end
