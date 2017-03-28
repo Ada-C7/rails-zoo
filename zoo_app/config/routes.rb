e6ec42da699d10b0467d1a29c27ceaeefa7312e6
Anacelia Gillette <acgillette426@gmail.com>
2017-03-28 13:04:17 -0700

    Made show route and template page

--- zoo_app/config/routes.rb
+++ zoo_app/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'animals#index'
   get '/animals', to: 'animals#index'
 
+  get 'animals/:id', to: 'animals#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
