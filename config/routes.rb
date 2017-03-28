bc1bfa875fe4d7921b9cb13c4ceb808febacf392
add2point71dots <alcland@gmail.com>
2017-03-27 19:32:11 -0700

    completed animals#show and the view for that

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'animals#index'
 
   get '/animals', to: 'animals#index'
+  get '/animals/:id', to: 'animals#show', as: 'animal'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
