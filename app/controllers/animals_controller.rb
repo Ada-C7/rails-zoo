2279a3d581b73824ad32a4ed523c6d8c4c8a8dc1
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-03-27 23:00:52 -0700

    Working on the edit/update action. Updated the routes.rb and the controller.rb

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -7,13 +7,33 @@ class AnimalsController < ApplicationController
     @animal = Animal.find(params[:id])
   end
 
+  def edit
+    @animal = Animal.find(params[:id])
+
+  end
+
+  def update
+    animal = Animal.find(params[:id])
+    if animal.update(animal_params)
+      redirect_to animals_path
+
+  end
+
   def new
 
   end
 
   def create
-    
+
   end
+
+  private
+  def animal_params
+    params.require(:animal).permit(:name, :species, :age, :image_link)
+
+  end
+
+
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get "/animals/new", to: "animals#new"
   post "animals", to: "animals#create"
 
+  get "/animals/:id/edit", to: "animals#edit",  as:"edit_task"
+  patch "animals", to: "animals#update"
+
+
   get "/animals", to: "animals#index"
 
   get "/animals/:id", to: "animals#show", as:"animal"
