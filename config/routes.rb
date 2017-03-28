6ee2bc9dd5b26bc413cbc2554bf0b46c107260ae
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-03-28 16:31:25 -0700

    fixed routes for index and show, wrote html and erb to see first version of index/all animals

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'animals/index'
 
-  get 'animals/show'
+  # get 'animals/index', to: 'animals#index', as: 'animals'
+
+  get 'animals', to: 'animals#index', as: 'animals'
+
+  # get 'animals/show', to: 'animals#index', as: 'animal'
+
+  get 'animals/:id', to: 'animals#show', as: 'animal'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
