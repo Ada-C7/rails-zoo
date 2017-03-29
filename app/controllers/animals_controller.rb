6d83c54cb30eb2d5a6321c0a1ffdebaf61802a06
Tamiko Terada <tamikodevo@gmail.com>
2017-03-29 00:19:09 -0700

    create: new and create controller methods

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,9 +1,19 @@
 class AnimalsController < ApplicationController
+  def new
+    @animal = Animal.new
+  end
+
   def index
     @animals = Animal.all
   end
 
   def show
     @animal = Animal.find(params[:id])
+  end
+
+  def create
+    animal = Animal.new(params[:animal])
+    animal.save
+    redirect_to animals_path
   end
 end
