ab795b299e36cba10a0f50338517c175c9c78ec1
Jackie Watanabe <sushiigirl@gmail.com>
2017-03-28 22:31:22 -0700

    created routes for adding new animal, created view file and controller action

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -6,4 +6,21 @@ class AnimalsController < ApplicationController
   def show
     @result_animal = Animal.find(params[:id])
   end
+
+  # def edit
+  #   @result_animal = Animal.find(params[:id])
+  # end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new name: params[:animal][:name], age: params[:animal][:age], birthday: params[:animal][:birthday], species: params[:animal][:species]
+
+    if @animal.save
+      redirect_to animals_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  get '/animals', to: 'animals#index', as: 'animals'
 
-  get '/animals/:id', to: 'animals#show', as: 'animal'
+
+  get '/animals/new', to: 'animals#new', as: 'new_animal'
+  post '/animals', to: 'animals#create'
+
+get '/animals', to: 'animals#index'
+get '/animals/:id', to: 'animals#show', as: 'animal'
+
+
+
+
+
+  # get '/animals/edit/:id', to: 'animals#edit', as: 'edit_animal'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
