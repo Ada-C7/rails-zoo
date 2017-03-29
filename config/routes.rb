7a19ebef94a71403cdd1ab4e92aca4f0ff6f0263
Brenna <bcmdarroch@gmail.com>
2017-03-28 18:34:43 -0700

    wrote edit, show, new pages HTML

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get '/animals/new', to: 'animals#new', as: 'new_animal'
 
-  post '/animals', to: 'animal#create'
+  post '/animals', to: 'animals#create'
 
   get '/animals/:id', to: 'animals#show', as: 'animal'
 
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   patch '/animals/:id', to: 'animals#update'
 
-  delete 'animals/:id', to: 'animals#destroy'
+  delete '/animals/:id', to: 'animals#destroy', as: 'delete_animal'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
