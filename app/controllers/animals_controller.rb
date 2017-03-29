6637012ba8b89b870bce6f44de9c3f47f0aeaa5d
Tamiko Terada <tamikodevo@gmail.com>
2017-03-29 00:36:17 -0700

    add: private method to whitelist user input

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,4 +1,5 @@
 class AnimalsController < ApplicationController
+
   def new
     @animal = Animal.new
   end
@@ -12,8 +13,13 @@ class AnimalsController < ApplicationController
   end
 
   def create
-    animal = Animal.new(params[:animal])
+    animal = Animal.new(user_params)
     animal.save
     redirect_to animals_path
   end
+
+private
+  def user_params
+     params.require(:animal).permit(:name, :species, :age)
+   end
 end
