e615a6b77b745f0bc0c38fc0a3fdb82e36a45567
amb54 <ambarklund@hotmail.com>
2017-03-28 18:44:52 -0700

    Created get animals route. Created a method index in the controller. Created a private method animal_params in the controller. Dreated a index.html.erb file in views/animals. The index view creates a simple list of the animals in the browser.

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,14 @@
 class AnimalsController < ApplicationController
+
+  def index
+    puts '>>>>>>amb<<<<<<<<: Inside animals#index'
+    @animals = Animal.all
+  end
+
+  #### PRIVATE
+  private
+  def animal_params
+    params.require(:animal).permit(:name, :species, :age)
+  end
+
 end
