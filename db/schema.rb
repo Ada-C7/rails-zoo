1634526c8f6ea465c46e0f2a8dcd3ae02c992398
Jackie Watanabe <sushiigirl@gmail.com>
2017-03-29 13:44:05 -0700

    created new migration to add new column for image_url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328233423) do
+ActiveRecord::Schema.define(version: 20170329203709) do
 
   create_table "animals", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170328233423) do
     t.date     "birthday"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image_url"
   end
 
 end
