1032b835523a1065d6d6ae8874fc0f62359277b4
Spatterjaaay <aja.pursova@gmail.com>
2017-03-29 15:26:31 -0700

    added routes, model methods and view pages to display the new animal form and creating new animal

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -6,4 +6,21 @@ class AnimalsController < ApplicationController
   def show
     @animal = Animal.find(params[:id])
   end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    animal = Animal.create(strong_params)
+    if animal.id
+      redirect_to animals_path
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:animal).permit(:name, :species, :age)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get "/animals", to:"animals#index"
+  get "/animals/new", to:"animals#new", as: "new_animal"
+  post "/animals", to:"animals#create"
 
+  get "/animals", to:"animals#index"
   get "/animals/:id", to:"animals#show", as: "animal"
 end
