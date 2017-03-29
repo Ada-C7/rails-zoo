84fc259447cb235ab89f4460b35f661353533f62
Ann Dai <gofarann@gmail.com>
2017-03-29 13:16:58 -0700

    added routes, methods, and views for to show and edit animals, added image assets

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,4 +1,28 @@
 class AnimalsController < ApplicationController
   def index
+    @animals = Animal.all
   end
+
+  def show
+    @animal = Animal.find params[:id]
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    animal = Animal.create animal_params
+    unless animal.id == nil
+      redirect_to animals_path #this will always default to the GET verb unless otherwise specified
+    end
+  end
+
+end
+
+
+private
+
+def animal_params #this is called a strong param, it is a technique that limits what parameters are used in certain methods
+  params.require(:animal).permit(:name, :age, :species, :fave_food)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+
+  get '/animals/new', to: 'animals#new', as: 'new_animal'
+
+  post '/animals', to: 'animals#create'
+
   get 'animals/index'
 
-  get 'books/index'
+  get '/animals', to: 'animals#index'
+
+  get '/animals/:id', to: 'animals#show', as: 'each_animal'
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
