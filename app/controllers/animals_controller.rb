646ea0edeb33ea4f96eaac2d0286315f80c19270
Theresa Manney <theresa.m.17@gmail.com>
2017-03-29 16:27:36 -0700

    Animal list shows all the animals I added from the rails console, each animal is a link that can be clicked on to see animal info

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,4 +1,11 @@
 class AnimalsController < ApplicationController
+
   def index
+    @animals = Animal.all
   end
+
+  def show
+    @result_animal = Animal.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'animals#index'
 
-  get 'animals/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/animals', to: 'animals#index'
+  get '/animals/:id', to: 'animals#show', as: 'animal'
+  
 end
