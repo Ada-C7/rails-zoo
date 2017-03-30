f3c34ae0f8c9bbd6fe3f07e8553e5055aeb14b28
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-03-30 11:03:10 -0700

    Created route, controller, and view to 'show' single animal

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -3,5 +3,8 @@ class AnimalsController < ApplicationController
   def index
     @animals = Animal.all
   end
-  
+
+  def show
+    @show_animal = Animal.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root "animals#index"
 
   get '/animals', to: 'animals#index'
-
+  get '/animals/:id', to: 'animals#show', as: 'animal'
 
 end
