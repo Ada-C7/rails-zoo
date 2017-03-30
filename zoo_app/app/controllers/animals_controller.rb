155de03b05cfdca4137ad64536bb4ca350f8f48b
Anacelia Gillette <acgillette426@gmail.com>
2017-03-30 14:47:53 -0700

    Made new and create routes and methods

--- zoo_app/app/controllers/animals_controller.rb
+++ zoo_app/app/controllers/animals_controller.rb
@@ -4,6 +4,23 @@ class AnimalsController < ApplicationController
   end
 
   def show
-    @animal = Animal.find(params[:id])
+    @result_animal = Animal.find(params[:id])
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    animal = Animal.create animal_params
+    if animal.save
+      redirect_to animals_path
+    end
+  end
+
+  private
+
+  def animal_params
+    params.permit(:name, :species, :age, :animaltype)
   end
 end
--- zoo_app/config/routes.rb
+++ zoo_app/config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   root 'animals#index'
   get '/animals', to: 'animals#index'
 
-  get 'animals/:id', to: 'animals#show'
+  get 'animals/new', to: 'animals#new'
+  post 'animals/', to: 'animals#create'
+
+
+  get 'animals/:id', to: 'animals#show', as: 'animal'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
