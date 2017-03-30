cbe350e26d5cdf4cd4662bf490a81c963c3fb557
Tamiko Terada <tamikodevo@gmail.com>
2017-03-30 14:27:55 -0700

    add: update/edit for an animal

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -18,6 +18,18 @@ class AnimalsController < ApplicationController
     redirect_to animals_path
   end
 
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    animal = Animal.find(params[:id])
+    animal.update_attributes(user_params)
+    animal.save
+
+    redirect_to animal_path(animal)
+  end
+
 private
   def user_params
      params.require(:animal).permit(:name, :species, :age)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
 get '/animals', to: 'animals#index', as: 'animals'
 get '/animals/new', to: 'animals#new', as: 'new_animal'
+
 get '/animals/:id', to: 'animals#show', as: 'animal'
+get 'animals/:id/edit', to: 'animals#edit', as: 'edit_animal'
 
 post '/animals', to: 'animals#create'
+patch 'animals/:id', to: 'animals#update'
 end
