4f5b2130329702cdca0ee76017274f5effd7b039
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-03-30 11:23:44 -0700

    Created route, controller, and view to 'create' 'new' animal

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -7,4 +7,23 @@ class AnimalsController < ApplicationController
   def show
     @show_animal = Animal.find(params[:id])
   end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    animal = Animal.create animal_params
+
+    unless animal.id == nil
+      redirect_to animals_path
+    end
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :species, :age)
+  end
+
 end
