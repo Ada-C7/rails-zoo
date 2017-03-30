a5b1770fe16b59ba8fda83f40e14fd8ae9354607
Ashtn <ashtonehorton@gmail.com>
2017-03-29 23:17:40 -0700

    initialized create controller#action, defined strong params

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,11 +1,25 @@
 class AnimalsController < ApplicationController
   def new
     @animal = Animal.new
-  end 
+  end
+  def create
+    animal = Animal.create animal_params
+    if Animal.save
+      redirect_to animals_path
+    else
+      render :new
+    end
+  end
   def index
     @animals = Animal.all
   end
   def show
     @animal = Animal.find(params[:id])
   end
+end
+
+private
+
+def animal_params
+  params.require(:animal).permit(:name, :species, :age)
 end
