4fa0077621cd8dce1e96c83dc8e06f9c891a624f
Ashtn <ashtonehorton@gmail.com>
2017-03-29 22:03:33 -0700

    created route to show one animal

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  root 'animals#index'
  get '/animals', to: 'animals#index'
+ get '/animals/:id', to: 'animals#show', as: 'animal'
 end
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
