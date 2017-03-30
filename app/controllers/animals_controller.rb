6ce170ba6c63877404ec8be05a132d917dede0ec
Regan Huff <regan.huff@gmail.com>
2017-03-30 13:30:24 -0700

    Add controller actions and view to create new animal

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -3,7 +3,27 @@ class AnimalsController < ApplicationController
     @animals = Animal.all
   end
 
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    animal = Animal.create animal_params
+
+    unless animal.id == nil
+      redirect_to animals_path
+    end
+  end
+
   def show
     @animal = Animal.find(params[:id])
   end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :species, :sci_name, :age, :image)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "animals#index"
 
+  get "/animals/new", to: "animals#new", as: "new_animal"
+  post "/animals", to: "animals#create"
+
   get "/animals", to: "animals#index"
   get "/animals/:id", to: "animals#show", as: "animal"
 
