4838d8010fb5064621a6c0dabc96b85d07f69251
Ting Wong <hellotingwong@gmail.com>
2017-03-31 14:33:44 -0700

    added editing ability

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -17,6 +17,25 @@ class AnimalsController < ApplicationController
     @animal = Animal.find(params[:id])
   end
 
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    animal = Animal.find(params[:id])
+    animal.update_attributes(animal_params)
+
+    redirect_to animal_path(animal.id)
+  end
+
+  def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+
+    redirect_to animals_path
+  end
+
+
 private
   def animal_params
     return params.require(:animal).permit(:name, :species, :age)
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   post 'animals', to: 'animals#create'
 
   get 'animals/:id', to: 'animals#show', as: 'animal'
+  get 'animals/:id/edit', to: 'animals#edit', as: 'edit_animal'
+  patch 'animals/:id', to: 'animals#update'
 
+  delete 'animals/:id', to: 'animals#destroy', as: 'delete_animal'
 end
