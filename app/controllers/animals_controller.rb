07ef3e71cb2effa2d148fcf0dbeec13a4050662e
laura <lmelgarejos@unal.edu.co>
2017-03-31 12:55:37 -0300

    Delete and edit actions added and working

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -3,7 +3,41 @@ class AnimalsController < ApplicationController
     @animals = Animal.all
   end
 
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    Animal.create(animal_params)
+
+    redirect_to animals_path
+  end
+
   def show
     @animal = Animal.find(params[:id])
+  end
+
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    animal = Animal.find(params[:id])
+    animal.update_attributes(animal_params)
+    animal.save
+
+    redirect_to animals_path(animal)
+  end
+
+  def delete
+    animal = Animal.find(params[:id])
+    animal.destroy
+
+    redirect_to animals_path
+  end
+
+  private
+  def animal_params
+    return params.require(:animal).permit(:name, :species, :age, :weight)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   get 'animals', to: 'animals#index', as: 'animals'
   get 'animals/:id', to: 'animals#show', as: 'animal'
 
+  get 'animals/new', to: 'animals#new', as: 'new_animal'
+  post 'animals', to: 'animals#create'
+
+  get 'animals/:id/edit', to: 'animals#edit', as: 'edit_animal'
+
+  post 'animals/:id/edit', to: 'animals#edit'
+  patch 'animals/:id', to: 'animals#update'
+  delete 'animals/:id', to: 'animals#delete'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
