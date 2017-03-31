481fcceb06e22d8c56112876696cab0988d7aa6a
Haby Randall <sow.haby@gmail.com>
2017-03-31 08:40:39 -0400

    added a show route, as well as a show view

--- rails_zoo/app/controllers/animals_controller.rb
+++ rails_zoo/app/controllers/animals_controller.rb
@@ -2,4 +2,10 @@ class AnimalsController < ApplicationController
   def index
     @animals = Animal.all
   end
+
+
+  def show
+    @result_animal = Animal.find(params[:id])
+  end
+
 end
--- rails_zoo/config/routes.rb
+++ rails_zoo/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   get 'animals/index'
   get '/animals', to: 'animals#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/animals/:id', to: 'animals#show', as: 'animal'
+
+
 end
