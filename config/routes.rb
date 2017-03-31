7a1d1469ad44618e5aeba24b287a0361ec6ce48a
laura <lmelgarejos@unal.edu.co>
2017-03-31 17:36:54 -0300

    Fixed ordered of routes, create a new animal working again

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   get 'animals', to: 'animals#index', as: 'animals'
-  get 'animals/:id', to: 'animals#show', as: 'animal'
-
   get 'animals/new', to: 'animals#new', as: 'new_animal'
   post 'animals', to: 'animals#create'
 
+  get 'animals/:id', to: 'animals#show', as: 'animal'
   get 'animals/:id/edit', to: 'animals#edit', as: 'edit_animal'
 
   post 'animals/:id/edit', to: 'animals#edit'
