af2466e83dc000ad27d574b01b4b2f10681efb49
Bo Trethewey <botrethewey@gmail.com>
2017-03-30 19:21:39 -0700

    Edit/update and delete actions added

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -6,4 +6,44 @@ class AnimalsController < ApplicationController
   def show
     @result_animal = Animal.find(params[:id])
   end
+
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    animal = Animal.find(params[:id])
+    if animal.update(animal_params)
+      redirect_to animal_path
+    end
+  end
+
+  def destroy
+    Animal.destroy(params[:id])
+    redirect_to animals_path
+  end
+
+
+
+
+
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    animal = Animal.create animal_params
+    unless animal.id == nil
+      redirect_to animals_path
+    end
+  end
+
+  private
+
+  #strong param
+  def animal_params
+    params.require(:animal).permit(:name, :species, :age)
+  end
+
 end
