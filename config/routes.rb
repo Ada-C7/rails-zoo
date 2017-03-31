f7b0691ff00b700e760f8880eea3f5b419b3cb57
Spatterjaaay <aja.pursova@gmail.com>
2017-03-31 16:15:00 -0700

    added confirmation if user truly wants to delete an animal, changed name of the route for edit form

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "/animals", to:"animals#index"
   get "/animals/:id", to:"animals#show", as: "animal"
 
-  get "/animals/:id/edit", to:"animals#edit", as: "update_animal"
+  get "/animals/:id/edit", to:"animals#edit", as: "edit_animal"
   patch "/animals/:id", to:"animals#update"
 
   delete "animals/:id", to:"animals#destroy", as: "delete_animal"
