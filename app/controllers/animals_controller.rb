d4b6771df618e4fb77ee943c284e3a3e2bb8918d
Hyunji Kim <ricecakemonster@gmail.com>
2017-03-31 17:24:13 -0700

    Added new animal function

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -6,4 +6,22 @@ class AnimalsController < ApplicationController
   def show
     @animal = Animal.find(params[:id])
   end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    a = Animal.create(animal_params)
+    a.save
+    redirect_to animals_path
+  end
+
+
+  private
+
+  def animal_params
+    return params.require(:animal).permit(:name, :species, :age, :size)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'animals#index'
   get 'animals', to: 'animals#index', as: 'animals'
+  get 'animals/new', to: 'animals#new', as: 'new_animal'
+  post 'animals', to: 'animals#create'
   get 'animals/:id', to: 'animals#show', as: 'animal'
+
 end
