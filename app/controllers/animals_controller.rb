c8c480f7c947fed8f8873cd49a56d2b7a35a8797
Sahana Murthy <s.murthy730.com>
2017-03-31 20:58:55 -0700

    Completed the add a new animal requirement. Requirement met and working correctly.

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -4,8 +4,24 @@ class AnimalsController < ApplicationController
     @animals = Animal.all
   end
 
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    animal_data = animal_params
+    Animal.create(animal_data)
+
+    redirect_to animals_path
+  end
+
   def show
     id = params[:id].to_i
     @animal = Animal.find(id)
+  end
+
+private
+  def animal_params
+    return params.require(:animal).permit(:name, :species, :age)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get 'animals', to: 'animals#index'
+
+  get 'animals/new', to: 'animals#new', as: 'new_animal'
+  post 'animals', to: 'animals#create'
+
   get 'animals/:id', to: 'animals#show', as: 'animal'
 end
