2733311bc2b8cf8a6835fa72cfaac58abaa12c13
amb54 <ambarklund@hotmail.com>
2017-04-02 16:44:32 -0700

    Added a image tag to the show view with subsequent rule set. Added a column image to the data base. Made some minor corrections to the link buttons in the show view.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170329000613) do
+ActiveRecord::Schema.define(version: 20170402232020) do
 
   create_table "animals", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170329000613) do
     t.integer  "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
