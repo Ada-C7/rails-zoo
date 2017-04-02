19cfd283bc5cd90f491e651cd6b64274540eebce
Danielle Birbal <birbalds@gmail.com>
2017-04-02 14:08:01 -0700

    Added Create animal functionality

--- rails-zoo/app/controllers/animals_controller.rb
+++ rails-zoo/app/controllers/animals_controller.rb
@@ -1,2 +1,47 @@
 class AnimalsController < ApplicationController
+    def index
+        @animals = Animal.all
+    end
+
+    def new
+        @animal = Animal.new
+    end
+
+    def create
+        new
+        if @animal.update(animal_params)
+            redirect_to animals_path
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        @animal = Animal.find(params[:id])
+    end
+
+    def update
+        animal = Animal.find(params[:id])
+        if animal.update(animal_params)
+            redirect_to animal_path(animal)
+        else
+            render 'edit'
+        end
+    end
+
+    def show
+        @animal = Animal.find(params[:id])
+    end
+
+    def destroy
+        animal = Animal.find(params[:id])
+        animal.destroy
+        redirect_to animals_path
+    end
+
+    private
+
+    def animal_params
+        params.require(:animal).permit(:name, :species, :age)
+    end
 end
--- rails-zoo/config/routes.rb
+++ rails-zoo/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'animals', to: 'animals#index'
+
+    get 'animals/new', to: 'animals#new', as: 'new_animal'
+    post 'animals', to: 'animals#create'
+
+    get 'animals/:id/edit', to: 'animals#edit', as: 'edit_animal'
+    patch 'animals/:id', to: 'animals#update'
+
+    get 'animals/:id', to: 'animals#show', as: 'animal'
+
+    delete 'animals/:id', to: 'animals#destroy'
 end
