3f84b07e3af9860f8a871e58c70603d1b0bbf44d
Maria McGrew <mcg.maria@gmail.com>
2017-04-02 03:40:38 -0700

    completed new and create method, updated add new animal form by adding form_for

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -3,11 +3,22 @@ class AnimalsController < ApplicationController
     @animals = Animal.all
   end
 
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    Animal.create(animal_params)
+
+    redirect_to animals_path
+  end
+
   def show
     @animal = Animal.find(params[:id])
   end
 
-  def new
-    @animal = Animal.new
+private
+  def animal_params
+    return params.require(:animal).permit(:name, :species, :age)
   end
 end
