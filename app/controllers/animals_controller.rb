c283a8e54d7ab9d614ae9773928d6ac588af5bd8
Cara Comfort <cara.comfort10@gmail.com>
2017-04-02 15:10:57 -0700

    Added new action

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -3,7 +3,29 @@ class AnimalsController < ApplicationController
     @animals = Animal.all
   end
 
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    Animal.create(animal_params)
+
+    redirect_to animals_path
+  end
+
   def show
     @animal = Animal.find(params[:id].to_i)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def animal_params # strong params
+    return params.require(:animal).permit(:name, :age, :species, :gender, :details)
   end
 end
