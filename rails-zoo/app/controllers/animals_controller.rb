a7c4a0ea4ea603e245196049b17c7a117672012f
EricaJCase <ericajcase@gmail.com>
2017-04-02 09:46:57 -0700

    Internet images added

--- rails-zoo/app/controllers/animals_controller.rb
+++ rails-zoo/app/controllers/animals_controller.rb
@@ -6,4 +6,19 @@ class AnimalsController < ApplicationController
   def show
     @animal = Animal.find(params[:id])
   end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    Animal.create(animal_params)
+    redirect_to animals_path
+  end
+
+  private
+  def animal_params
+    return params.require(:animal).permit(:name, :species, :age)
+  end
+
 end
