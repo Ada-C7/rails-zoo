d7dec976c5f3db08f03345947caafac126344e20
Andrea Valliere <amvalliere@gmail.com>
2017-04-02 22:09:12 -0700

    Added new.html.erb and created routes and controller methods to allow user to add new animal

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -9,4 +9,23 @@ class AnimalsController < ApplicationController
     @animal = Animal.find(id)
   end
 
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    puts ">>>> AMV: In AnimalsController#create"
+
+    animal_data = animal_params
+    puts animal_data.to_hash
+    Animal.create(animal_data)
+
+    redirect_to animals_path
+  end
+
+  private
+    def animal_params
+      return params.require(:animal).permit(:name, :species, :age)
+    end
+
 end
