6ef6ee3c68f804c8785b2ed0c591c63a87d08ab3
Alix Hamilton <alix.hamilton@gmail.com>
2017-04-02 22:16:08 -0700

    adds basic style and local default photos

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,17 +1,20 @@
 class Animal < ApplicationRecord
-  SPECIES_DATA = {
-    sockeye: {scientific_name: "Oncorhynchus nerka", default_picture: "https://upload.wikimedia.org/wikipedia/commons/8/88/Sockeye_adult_male.jpg"},
-    coho: {scientific_name: "Oncorhynchus kisutch", default_picture: "https://upload.wikimedia.org/wikipedia/commons/9/91/Oncorhynchus_keta.jpeg"},
-    chinook: {scientific_name: "Oncorhynchus tshawytscha", default_picture: "https://upload.wikimedia.org/wikipedia/commons/d/d8/Chinook_Salmon_Adult_Male.jpg"},
-    pink: {scientific_name: "Oncorhynchus gorbuscha", default_picture: "https://upload.wikimedia.org/wikipedia/commons/c/c2/Humpback_Salmon_Adult_Male.jpg"},
-    chum: {scientific_name: "Oncorhynchus keta", default_picture: "https://upload.wikimedia.org/wikipedia/commons/7/71/Dog_Salmon_Breeding_Male.jpg"}
+  
+  SALMON_SPECIES = {
+    sockeye: "Oncorhynchus nerka",
+    coho: "Oncorhynchus kisutch",
+    chinook: "Oncorhynchus tshawytscha",
+    pink: "Oncorhynchus gorbuscha",
+    chum: "Oncorhynchus keta",
+    steelhead: "Oncorhynchus mykiss",
+    cutthroat: "Oncorhynchus clarkii"
   }
 
   def initialize(params={})
     super
     unless params[:species].nil? || params[:species].empty?
-      self.scientific_name = SPECIES_DATA[params[:species].to_sym][:scientific_name]
-      self.photo_url = SPECIES_DATA[params[:species].to_sym][:default_picture] if self.photo_url.empty?
+      self.scientific_name = SALMON_SPECIES[params[:species].to_sym]
+      self.photo_url = "#{params[:species]}-salmon" if self.photo_url.empty?
     end
   end
 end
